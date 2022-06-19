54bdfe7f5b7a680758065bcd3f01aeb5f503892d
brestain <56471813+brestain@users.noreply.github.com>
2022-06-19 22:11:06 +0800

    Initial commit

---
+++ Helloworld.java
@@ -0,0 +1,9 @@
+public class Helloworld{
+    /* 第一个Java程序
+     * 它将输出字符串 Hello World
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World"); // 输出 Hello World
+    }
+}
+//tyfdjdtfjc 
